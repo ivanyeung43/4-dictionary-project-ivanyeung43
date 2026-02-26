8f2cc5b80b49e9d6507fe6205981d20234b09397
ivanyeung43 <ivany43@nycstudents.net>
2026-02-26 08:42:35 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+item = {
+    "name": "Samsung 55\" 4K UHD TV",
+    "price": 429.99,
+    "department": "Televisions",
+    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+}
+item = {
+    "name": "Desktop",
+    "price": 929.99,
+    "department": "Electronics",
+    "description": "RYZEN 5 5600 W/ NVIDIA RTX 3060TI"
+}
+
+best_buy_items[1]["name"]
