fca0fda23602a8df02120c701bdb2267dd027034
ivanyeung43 <ivany43@nycstudents.net>
2026-02-27 09:25:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,24 @@ for index, item in enumerate(item):
 user_purchase = input("Select one item to purchase")
 
 if user_purchase == "0":
-    item[0]["name"]
+    item[0]["name"]["price"]
 elif user_purchase == "1":
-    item[1]["name"]
+    item[1]["name"]["price"]
 elif user_purchase == "2":
-    item[2]["name"]
+    item[2]["name"]["price"]
 else:
     print("Select 0, 1 or 2")
 
+
+""" # Practice Assessment
+c = "c"
+e = "."
+def occupied(x,y,t):
+    found = 0
+    for i in range(x):
+        if (y[i]) == c and t[i] == c:
+            found += 1
+    print(found)
+            
+occupied(5,[e,c,c,c,c], [c,c,c,c,e])
+ """
