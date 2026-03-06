1dbe829fdbbdfa8d1444f1a70bc9a9345e5d7a90
ivanyeung43 <ivany43@nycstudents.net>
2026-03-06 09:24:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 items = [
     {
         "name": "RADEON XFX SWIFT RX 9070XT", 
-        "price": "$749.99", 
+        "price": "$759.99", 
         "description": "325mm LENGTH," "16GB VRAM" "COLOR:BLACK/WHITE"
     },
     {
@@ -11,23 +11,29 @@ items = [
     },
     {
         "name": "GIGBYTE AORUS MASTER 5080",
-        "price": "$1299.99",
+        "price": "$1199.99",
         "description": "360mm LENGTH, 16GB VRAM, COLOR:BLACK"
     },
 ]
 
-for index, items in enumerate(items):
-    print(index, ":", items["name"])
-
-user_purchase = int(input("Select one item to purchase"))
-
-
-if user_purchase == 0:
-    print(items[0]["name"])
-elif user_purchase == 1:
-    print(items[1]["name"])
-elif user_purchase == 2:
-    print(items[2]["name"])
+for index, item in enumerate(items):
+    print(index, ":", item["name"])
+shop = ""
+cart = []
+user_purchase = int(input("Select one item to purchase: Type Checkout to end shopping"))
+while shop != "Checkout":
+    if user_purchase == 0:
+        print(items[0]["name"])
+        cart.append(items[0]["name"])
+    elif user_purchase == 1:
+        print(items[1]["name"])
+        cart.append(items[1]["name"])
+    elif user_purchase == 2:
+        print(items[2]["name"])
+        cart.append(items[2]["name"])
+    print(cart)
+print("Shopping has ended")
+print(cart)
 
 
 
